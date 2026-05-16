df9df5281fa6d10f884675b83d484aae33b7dbbd
Duskell <duskell@proton.me>
2026-05-16 13:45:16 +0200

    possibly fixed i3 error

--- modules/desktop/de/i3.nix
+++ modules/desktop/de/i3.nix
@@ -119,7 +119,7 @@ in
             outer = 5;
           };
 
-          keybindings = lib.mkOptionDefault {
+          keybindings = {
             "XF86AudioMute"         = "exec wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
             "XF86AudioLowerVolume"  = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 4%-";
             "XF86AudioRaiseVolume"  = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 4%+";
