2744100c49086ea428ba126f3633ae28587a8234
Duskell <duskell@proton.me>
2026-07-08 01:02:56 +0200

    fixed typo

--- modules/programs/xarchiver.nix
+++ modules/programs/xarchiver.nix
@@ -5,17 +5,17 @@
   framework,
   ...
 }: let
-  inherit (lib) mkIf mkMerge mkDefault;
+  inherit (lib) mkIf mkEnableOption;
   cfg = config.framework.programs.xarchiver;
   primaryUser = config.framework.primaryUser;
 in {
   options.framework.programs.xarchiver = {
-    enable = lib.mkEnableOption "install xArchiver";
-    default = lib.mkEnableOption "add xArchiver as the default compressed-archive handler" // {default = true;};
+    enable = mkEnableOption "install xArchiver";
+    default = mkEnableOption "add xArchiver as the default compressed-archive handler" // {default = true;};
   };
 
-  config = lib.mkIf cfg.enable {
-    home-manager.user.${primaryUser} = {
+  config = mkIf cfg.enable {
+    home-manager.users.${primaryUser} = {
       home.packages = [pkgs.xarchiver];
     };
 
