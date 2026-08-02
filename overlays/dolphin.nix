a5b273a0db1d96bcee6195610aae5f6b90899ed6
Duskell <duskell@proton.me>
2026-08-02 16:30:42 +0200

    Changed the dolphin overlay to only use the kde kservice instead of libsForQt5

--- overlays/dolphin.nix
+++ overlays/dolphin.nix
@@ -17,8 +17,8 @@ final: prev: {
       postBuild = ''
         rm $out/bin/dolphin
         makeWrapper ${kprev.dolphin}/bin/dolphin $out/bin/dolphin \
-          --set XDG_CONFIG_DIRS "${prev.libsForQt5.kservice}/etc/xdg:$XDG_CONFIG_DIRS" \
-          --run "${kfinal.kservice}/bin/kbuildsycoca6 --noincremental ${prev.libsForQt5.kservice}/etc/xdg/menus/applications.menu"
+          --set XDG_CONFIG_DIRS "${kprev.kservice}/etc/xdg:$XDG_CONFIG_DIRS" \
+          --run "${kprev.kservice}/bin/kbuildsycoca6 --noincremental ${prev.libsForQt5.kservice}/etc/xdg/menus/applications.menu"
       '';
       passthru = (kprev.dolphin.passthru or {}) // {dev = kprev.dolphin.dev;};
     };
